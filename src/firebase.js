68ec247b5b761fc26ad357a9188dfd8ee2c863f7
DariusGalu <galu.darius77@gmail.com>
2020-12-01 09:26:08 +0200

    init

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCA3N1hVvSOY5MnOP1tqnRiKzZcQ_UFWmg",
+    authDomain: "whatsapp-mern-91830.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-91830.firebaseio.com",
+    projectId: "whatsapp-mern-91830",
+    storageBucket: "whatsapp-mern-91830.appspot.com",
+    messagingSenderId: "573845076484",
+    appId: "1:573845076484:web:8fe5357926fdf8af225351"
+  };
